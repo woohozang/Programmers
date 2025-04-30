5b3ceefbdee93abad61350d54bb66c5451587bd1
심우호 <71766148+woohozang@users.noreply.github.com>
2025-04-30 23:14:28 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.02 ms, Memory: 3.73 MB -BaekjoonHub

---
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int a, int b) {
+    int answer = 0;
+    string ab = to_string(a) + to_string(b);
+    string ba = to_string(b) + to_string(a);
+    
+    answer = (stoi(ab)>stoi(ba))? stoi(ab) : stoi(ba);
+    return answer;
+}

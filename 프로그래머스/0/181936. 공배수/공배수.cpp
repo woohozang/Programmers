7a76be1d8decf13ea7c4fd183be23842bb7863ad
심우호 <71766148+woohozang@users.noreply.github.com>
2025-05-01 21:49:12 +0900

    [level 0] Title: 공배수, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/0/181936. 공배수/공배수.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int number, int n, int m) {
+    int answer = 0;
+    if(number % n ==0 && number % m==0){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+    return answer;
+}
